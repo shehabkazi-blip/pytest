186513226e7c63d55eb5078acd8fb6a3c104a346
Nure Siddiq <nalamsiddiq@gmail.com>
2026-06-19 22:57:40 +0600

    Update test case

--- test_calculator.py
+++ test_calculator.py
@@ -2,7 +2,7 @@ import pytest
 from calculator import add, subtract, multiply, divide
 
 def test_add():
-    assert add(2, 3) == 7
+    assert add(2, 3) == 5
 
 def test_subtract():
     assert subtract(5, 3) == 2
