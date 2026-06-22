ab141371fe923f7c4b74402003fc1b31b1e434a8
Md Shehab Kazi <shehabkazi@gmail.com>
2026-06-23 01:27:13 +0600

    Spacing issue resolved

--- test_calculator.py
+++ test_calculator.py
@@ -1,21 +1,26 @@
 import pytest
 from calculator import add, subtract, multiply, divide
 
+
 def test_add():
     assert add(2, 3) == 5
+
 
 def test_subtract():
     assert subtract(5, 3) == 2
     assert subtract(0, 5) == -5
+
 
 def test_multiply():
     assert multiply(2, 3) == 6
     assert multiply(-2, 3) == -6
     assert multiply(0, 5) == 0
 
+
 def test_divide():
     assert divide(6, 3) == 2
     assert divide(-6, 3) == -2
+
 
 def test_divide_by_zero():
     with pytest.raises(ValueError):
